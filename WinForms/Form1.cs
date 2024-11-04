b29bc73b6597b1c25357738bbefa6993d9cfeeb0
plesnikjakub <jakub.plesnik@gmail.com>
2024-11-04 17:26:10 +0100

    add winform

---
+++ WinForms/Form1.cs
@@ -0,0 +1,16 @@
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Hello World!";
+            textBox2.Text = "Hello World! 2";
+        }
+    }
+}
